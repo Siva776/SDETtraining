b5f15b2a9517a8ec1829b5440af013912793edae
siva sathya putta <sathya@MacBookPro.attlocal.net>
2024-08-12 19:59:04 -0500

    this is my first commit

---
+++ src/com/training/java/Firstproject.java
@@ -0,0 +1,17 @@
+package com.training.java;
+
+public class Firstproject {
+
+	public static void main(String[] args) {
+		int age = 25;
+		long address = 124876;
+		short shortnum = 127;
+		double num= 1.2368;		
+System.out.println(age);
+System.out.println(address);
+System.out.println(shortnum);
+System.out.println(num);
+
+	}
+
+}
