ca2874c426163b3aa540310d3c10bd33a1d08838
siva <puttasivasathya@gmail.com>
2024-08-17 10:32:43 -0500

    loops

---
+++ src/com/training/java/loops.java
@@ -0,0 +1,34 @@
+package com.training.java;
+
+public class loops {
+
+	public static void main(String[] args) {
+		System.out.println("odd numbers");
+		   
+       for (int i = 0; i <= 100; i++) {
+            if (i % 2 != 0) {
+                System.out.println(i);
+            }
+        }
+       System.out.println("even numbers");
+	   
+       for(int i = 0; i <= 100; i++){
+                
+            if (i % 2 == 0) {
+                	
+           System.out.println(i);
+            }
+       }
+			
+			
+		
+			
+		
+		
+	}
+        
+ 
+}
+        
+	
+	
